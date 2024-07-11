c613deb5c77a9f73dcd1978ace8975c673a98c73
Andrx <aamelendez@scl.edu.gt>
2024-07-11 11:29:43 -0600

    cambios de link de header

--- js/header.js
+++ js/header.js
@@ -1,2 +1,2 @@
 let header = document.querySelector(".header")
-header.innerHTML=' <p>aamelendez@</p>     <div class="git"> GitGub  <a href="https://github.com/Aandreeemele/Evaluacion-periodo2"></a></div>';
+header.innerHTML=' <p>aamelendez@</p>     <div class="git"><a href="https://github.com/Aandreeemele/Evaluacion-periodo2">GitGub</a></div>';
