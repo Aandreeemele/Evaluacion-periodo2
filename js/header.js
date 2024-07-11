705373dfac4e18b916fc4e1fc2c8702dbaab1b00
Andrx <aamelendez@scl.edu.gt>
2024-07-11 10:39:41 -0600

    version header terminada

---
+++ js/header.js
@@ -0,0 +1,2 @@
+let header = document.querySelector(".header")
+header.innerHTML=' <p>aamelendez@</p>     <div class="git"> GitGub  <a href="https://github.com/Aandreeemele/Evaluacion-periodo2"></a></div>';
